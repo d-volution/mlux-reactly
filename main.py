a38d484bbb050ecdd5cc10d4fefcaa4d50ce7d5e
Donovan <it.don@pm.me>
2025-12-03 20:00:42 +0100

    use agent facade

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from mlux_reactly.react import run_react
 from mlux_reactly.types import LLM
+from mlux_reactly.tools import CalculatorTool
+from mlux_reactly import ReactlyAgent
 
 print("start")
 
 llm = LLM("qwen2.5:7b-instruct-q8_0")
+agent = ReactlyAgent(llm = llm, tools=[CalculatorTool()])
 
-print(run_react("Is 20 larger than 3?", llm, {}))
-print(run_react("How high is the Eiffel Tower?", llm, {}))
+print(agent.query("Is 20 larger than 3?"))
+print(agent.query("How high is the Eiffel Tower?"))
+print(agent.query("What is the square root of sin(0.1274)?"))
--- mlux_reactly/facade.py
+++ mlux_reactly/facade.py
@@ -1,6 +1,6 @@
 from typing import Any, Callable, Dict, List, Sequence, Optional, Union, Type, cast
 from .types import Tool, LLM
-
+from .react import run_react
 
 class ReactlyAgent:
     name: str
@@ -13,9 +13,12 @@ class ReactlyAgent:
         llm: LLM,
         name: str = "ReactlyAgent",
         description: str = "",
-        tools: list[Tool] = [],
+        tools: List[Tool] = [],
     ):
         self.name = name
         self.description = description
         self.llm = llm
         self.tools = {tool.name: tool for tool in tools}
+
+    def query(self, user_query: str) -> str:
+        return run_react(user_query, llm=self.llm, tools=self.tools)
