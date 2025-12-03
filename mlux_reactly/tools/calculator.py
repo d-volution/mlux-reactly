d172cff3cfc50acca1d4ad9496ca4779de4c2fe0
Donovan <it.don@pm.me>
2025-12-03 20:00:57 +0100

    calculator tool: initial

---
+++ mlux_reactly/tools/calculator.py
@@ -0,0 +1,32 @@
+from typing import Any
+import numexpr as ne
+from ..types import Tool
+
+class CalculatorTool(Tool):
+    _name: str
+    _desc: str
+    
+    def __init__(self):
+        self._name = "calculator"
+        self._desc = """
+This calculator tool evaluates an arithmetic expression and returns the result. It takes as input a JSON-encoded object and returns the result as a number. It uses numexpr.
+
+Us the tool the following way:
+---
+Action: calculator
+Action Input: {{ "input": "your expression to be evaluated by numexpr" }}
+---
+"""
+
+    @property
+    def name(self) -> str:
+        return self._name
+    
+    @property
+    def desc(self) -> str:
+        return self._desc
+
+    def run(self, input: dict[str, Any]) -> tuple[str, bool]:
+        expr = input["input"]
+        result = ne.evaluate(expr)
+        return result, True
