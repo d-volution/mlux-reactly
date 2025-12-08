3cec5168f43b1de7868d59aea9fcb01295a1111b
Donovan <it.don@pm.me>
2025-12-08 23:59:35 +0100

    prompt: generate for subagent

--- mlux_reactly/prompts.py
+++ mlux_reactly/prompts.py
@@ -3,22 +3,8 @@ from .types import Tool
 # PROMPT TAKEN PARTIALLY FROM https://developers.llamaindex.ai/python/examples/agent/react_agent
 
 def generate_react_prompt(tools: dict[str, Tool]):
-
     tool_names = list(tools.keys())
-
-    tool_desc = "You have access to no tools.\n\n"
-    if len(tools) > 0:
-        tool_desc = """You have access to a wide variety of tools. You are responsible for using the tools in any sequence you deem appropriate to complete the task at hand.
-This may require breaking the task into subtasks and using different tools to complete each subtask.
-
-You have access to the following tools:
-
-
-"""
-        for tool_name, tool in tools.items():
-            tool_desc += f"### `{tool_name}`\n"
-            tool_desc += tool.description
-
+    tool_desc = generate_tool_descr(tools)
     return f"""
 You are designed to help with a variety of tasks, from answering questions to providing summaries to other types of analyses.
 
@@ -64,4 +50,70 @@ Answer: [your answer here (In the same language as the user's question)]
 ## Current Conversation
 
 Below is the current conversation consisting of interleaving human and assistant messages.
+"""
+
+
+
+def generate_tool_descr(tools: dict[str, Tool]) -> str:
+    if len(tools) == 0:
+        return "You have access to no tools.\n\n"
+    else:
+        tool_desc = """You have access to a wide variety of tools. You are responsible for using the tools in any sequence you deem appropriate to complete the task at hand.
+This may require breaking the task into subtasks and using different tools to complete each subtask.
+
+You have access to the following tools:
+
+
+"""
+        for tool_name, tool in tools.items():
+            tool_desc += f"### `{tool_name}`\n"
+            tool_desc += tool.description
+        return tool_desc
+
+
+
+
+
+def generate_subagent_prompt(tools: dict[str, Tool]):
+    tool_desc = generate_tool_descr(tools)
+    tool_names = [tool_name for tool_name in tools]
+    return f"""You are a subagent helping another meta-agent with some task.
+
+## Tools
+
+{tool_desc}
+
+
+## Output Format
+
+Use the following format:
+
+```
+Thought: your thoughts go here. Do not have to follow any format (just stay in one line). e.g.: Using tool x to find out y
+Action: tool name (one of {tool_names}) if using a tool.
+Action Input: the input to the tool, in a JSON format representing the kwargs (e.g. {{"input": "hello world", "num_beams": 5}})
+```
+
+Please ALWAYS start with a Thought.
+
+NEVER surround your response with markdown code markers. You may use code markers within your response if you need to.
+
+Please use a valid JSON format for the Action Input. Do NOT do this {{'input': 'hello world', 'num_beams': 5}}.
+
+If this format is used, the tool will respond in the following format:
+
+```
+Observation: tool response
+```
+
+You should keep repeating the above format till you have enough information to answer the meta-agent question/task without using any more tools. At that point, you MUST respond with the following format:
+
+```
+Thought: I can answer without using any more tools.
+Answer: Provide your answer (solution for the task from the meta-agent) here. The meta-agent will only get this answer as the result, not the entire conversation. 
+```
+
+## Current Conversation
+
+Below is the current conversation consisting of interleaving runtime and assistant messages.
 """
