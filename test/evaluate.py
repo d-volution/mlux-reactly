210d76f9c10d5988b6db54a178a6570e77b9b7ba
Donovan <it.don@pm.me>
2025-12-17 23:38:46 +0100

    feat: evaluate with simple statistics

--- test/evaluate.py
+++ test/evaluate.py
@@ -1,42 +1,63 @@
 import math
 import random
+import re
+import numexpr as ne
 from io import StringIO
 from mlux_reactly import ReactlyAgent, Recorder
-from mlux_reactly.types import LLM
+from mlux_reactly.types import LLM, RunConfig, Diagnostics
 from mlux_reactly.tools import CalculatorTool
 
 random.seed(12042)
+record_file=open("reactly_eval_query_record.jsonl", "+a")
+llm = LLM("qwen2.5:7b-instruct-q8_0")
 
 
-def test_examples(agent: ReactlyAgent) -> None:
+
+
+def calc_1_sinus_of(a):
+    return f"What is the sine of {a}?", str(ne.evaluate(f"sin({a})"))
+
+def calc_1_sinus_of_degrees(a):
+    return f"What is the sine of {a} degrees?", str(ne.evaluate(f"sin(deg * pi / 180)", {"deg": a, "pi": math.pi}))
+
+
+
+def evaluate_calc_1():
     examples = [
         calc_1_sinus_of,
         calc_1_sinus_of_degrees
     ]
+    stats = []
 
     for example in examples:
-        for _ in range(3):
-            agent.runConfig.stream = StringIO()
+        per_ex_stats = {
+            'example_name': example.__name__,
+            'nr_correct': 0,
+            'nr_incorrect': 0
+        }
+
+        for _ in range(10):
+            agent = ReactlyAgent(llm = llm, tools=[CalculatorTool()])
+            agent.runConfig = RunConfig(Diagnostics(), StringIO(), Recorder(f"eval_{example.__name__}", record_file))
             a = random.randint(-100000000, 100000000)
             question, truth = example(a)
+
             res = agent.query(question + " Answer with the result as a number only.")
-            print("==> ", question, " --> ", res, " -- ", truth)
-            if res != truth:
-                print(agent.runConfig.stream.getvalue())
-                print("--------------------------\n")
+
+            match_nr = re.search(r"-?\d+(\.\d+)?", res).group()
+            correct = match_nr == truth[:len(match_nr)]
+
+            print(f"==> Q: '{question}' res: {match_nr} truth: {truth} => {"correct" if correct else "incorrect"}")
+            if correct:
+                per_ex_stats['nr_correct'] += 1
+            else:
+                per_ex_stats['nr_incorrect'] += 1
+                #rint(agent.runConfig.stream.getvalue())
+                #rint("\n")
+
+        print("------------------------------------------------------------------")
+        for key, val in per_ex_stats.items():
+            print(f"  {key}: {val}")
 
 
-def calc_1_sinus_of(a):
-    return f"What is the sine of {a}?", str(math.sin(a))
-
-def calc_1_sinus_of_degrees(a):
-    return f"What is the sine of {a} degrees?", str(math.sin(math.radians(a)))
-
-
-# ---------------------------------------------------------------
-
-llm = LLM("qwen2.5:7b-instruct-q8_0")
-math_agent = ReactlyAgent(llm = llm, tools=[CalculatorTool()], 
-                          recorder=Recorder("calc1", file=open("reactly_eval_query_record.jsonl", "+a")))
-
-test_examples(math_agent)
+evaluate_calc_1()
