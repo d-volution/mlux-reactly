2b810c20ef10ac573db81325fa65f6e4446e4145
Donovan <it.don@pm.me>
2025-12-31 16:33:48 +0100

    refactor: use tools from folder

--- test/main.py
+++ test/main.py
@@ -1,44 +1,17 @@
 from mlux_reactly import ReactlyAgent, NormalTracer
-from mlux_reactly.types import LLM
-from typing import Annotated, List
-import json
 import sys
 import io
-
-print("start")
+from tools import calculator, text_count
 
 class StdoutStringIO(io.StringIO):
     def write(self, s):
         sys.stdout.write(s)
         return len(s)
 
-def candle_light(candle_type: str):
-    """This tool calculates the brightness for a candle light of some candle type."""
-    return
 
-def history_calendar(historic_event: str, precision_in_hours: int):
-    """A tool to query the date some historic event occurred."""
+tracer = NormalTracer(stream=StdoutStringIO(), record_file=open("reactly_query_record.jsonl", "+a"))
+agent = ReactlyAgent(tools=[calculator, text_count], tracer=tracer)
 
-def calculator(expression: Annotated[str, "some kind of math expr"]):
-    """A tool to evaluate a math expression."""
-
-def geo_3d(from_system: str, to_system: str, from_coordinates: str):
-    """A tool to convert between different kinds of earth coordinate systems."""
-
-agent = ReactlyAgent(tools=[candle_light, history_calendar, geo_3d, calculator], 
-                     tracer=NormalTracer(stream=StdoutStringIO(), 
-                                         record_file=open("reactly_query_record.jsonl", "+a")))
-
-
-"""
-print(agent.query("Is 20 larger than 3?"))
-print(agent.query("How high is the Eiffel Tower?"))
-print(agent.query("What is the square root of sin(0.1274)?"))
-print(agent.query("How many characters does the word rasperry have?"))
-print(agent.query("What degrees does Philip J. Pierre holds from the University of the West Indies?"))
-"""
-
-#print(agent.query("What does, if anything, Philip J. Pierre and Anton Zeilinger have in common? Try invoke at least one subagent before answering this."))
 
 def query(user_question: str):
     print(">>> " + user_question)
@@ -46,4 +19,11 @@ def query(user_question: str):
     print(response + "\n")
 
 
-query("How many years passed between when Napoleon became King of Italy and when the first world war ended?")
+
+query("How many characters does the word asinusobelix have?")
+query("What is the square root of 358079929?")
+query("Is 20 larger than 3?")
+query("How high is the Eiffel Tower?")
+query("What is the square root of sin(0.1274)?")
+query("How many characters does the word rasperry have?")
+query("What degrees does Philip J. Pierre holds from the University of the West Indies?")
