e541d41bd1add4edecdb8336933ec9d32a041ad1
Donovan <it.don@pm.me>
2025-12-31 17:44:44 +0100

    feat: chat implementation

--- test/chat.py
+++ test/chat.py
@@ -1,9 +1,23 @@
-from mlux_reactly import ReactlyAgent, Recorder
+import sys
+import io
+from mlux_reactly import ReactlyAgent, NormalTracer
 from mlux_reactly.types import LLM
+from tools import calculator, text_count
 
+
+class StdoutStringIO(io.StringIO):
+    trace_setting: bool = True
+
+    def write(self, s):
+        if self.trace_setting:
+            sys.stdout.write(s)
+        return len(s)
+
+stream = StdoutStringIO()
+tracer = NormalTracer(stream=stream, record_file=open("reactly_query_record.jsonl", "+a"))
 
 llm = LLM("qwen2.5:7b-instruct-q8_0")
-agent = ReactlyAgent(llm = llm, tools=[], recorder=Recorder("testchat", file=open("reactly_query_record.jsonl", "+a")))
+agent = ReactlyAgent(tools=[calculator, text_count], tracer=tracer, llm=llm)
 
 
 # chat loop:
@@ -14,8 +28,15 @@ while True:
     if user_input == "":
         continue
 
-    if user_input.startswith("/bye"):
-        break
+    if user_input.startswith("/"):
+        if user_input == "/bye" or user_input == "/q":
+            break
+        if user_input.startswith("/trace on"):
+            stream.trace_setting = True
+        if user_input.startswith("/trace off"):
+            stream.trace_setting = False
+
+        continue
 
     response = agent.query(user_input)
-    print(response)
+    print(response + "\n")
