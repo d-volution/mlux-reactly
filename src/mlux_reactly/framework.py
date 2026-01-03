3e6dfa99f07e25a1707bd591373321e00bd5acc9
Donovan <it.don@pm.me>
2026-01-03 17:43:42 +0100

    refactor: framework with dict for input data

--- src/mlux_reactly/framework.py
+++ src/mlux_reactly/framework.py
@@ -1,9 +1,9 @@
-from typing import Callable, Any, Tuple, List
+from typing import Callable, Any, Tuple, List, Dict
 from dataclasses import dataclass, asdict, is_dataclass
 from enum import Enum
 import json
 import ollama
-from .types import LLM, Tracer
+from .types import LLM, Tracer, ZeroTracer
 
 class Role(Enum):
     System = "system"
@@ -58,42 +58,51 @@ def serialize_data(data, encoding: Encoding) -> str:
     else:
         return str(data)
 
-def format_data_explicit(data, preshape_fn: Callable[[Any], Any] | None, encoding: Encoding) -> str:
-    data_preshaped = data if preshape_fn == None else preshape_fn(data)
+def format_data_explicit(data, preshape_fn: Callable[[Any], Any] | None, encoding: Encoding, *, ctx: str = "") -> str:
+    try:
+        data_preshaped = data if preshape_fn is None else preshape_fn(data)
+    except BaseException as e:
+        e.add_note(f"in {ctx}: format_data_explicit")
+        raise e
     return serialize_data(data_preshaped, encoding)
 
-def format_data(data, ff: FFFF) -> str:
-    return format_data_explicit(data, ff.preshape_fn, ff.encoding)
+def format_data(data, ff: FFFF, *, ctx: str = "") -> str:
+    return format_data_explicit(data, ff.preshape_fn, ff.encoding, ctx=ctx)
 
 
 def make_ffff(template: Any, encoding = Encoding.JSON, rules = [], preshape_fn = None) -> FFFF:
     return FFFF(serialize_data(template, encoding), encoding=encoding, rules=rules, preshape_fn=preshape_fn)
 
-def ffff_as_list(ff: FFFF) -> FFFF:
+def ffff_as_list(ff: FFFF, *, add_rules: List[str] = []) -> FFFF:
     return FFFF(
         format_template=f"[{ff.format_template}, ...]",
         encoding=Encoding.JSON,
-        rules=ff.rules
+        rules=ff.rules + add_rules,
+        preshape_fn=(lambda elements: [ff.preshape_fn(element) for element in elements]) if ff.preshape_fn is not None else None
     )
 
-
-def generate_conversation(*args, inputs: List[Tuple[str, FFFF]], output: Tuple[str, FFFF], with_output: bool = False):
-    required_len_of_args = len(inputs) + (1 if with_output else 0)
-    if len(args) != required_len_of_args:
-        raise ValueError(f"incompatible number of input arguments. should be {required_len_of_args}, is {len(args)}")
+def generate_conversation(data: Dict[str, Any], inputs: List[Tuple[str, FFFF]], output: Tuple[str, FFFF], with_output: bool = False, ctx: str = ""):
+    unhandled_input_labels = set(data.keys())
 
     conversation_section: str = ""
-    for i in range(len(inputs)):
-        input_value = args[i]
-        input_label = inputs[i][0]
-        input_ff = inputs[i][1]
-        conversation_section += f"{input_label}: {format_data(input_value, input_ff)}\n"
-    
+    for input in inputs:
+        input_label = input[0]
+        input_ff = input[1]
+        input_value = data.get(input_label) # TODO default
+        unhandled_input_labels.remove(input_label)
+        conversation_section += f"{input_label}: {format_data(input_value, input_ff, ctx=ctx+f": generate_conversation input {input_label}")}\n"
+
     output_label = output[0]
     output_ff = output[1]
     conversation_section += f"{output_label}: "
     if with_output:
-        conversation_section += format_data(args[len(inputs)], output_ff)
+        output_value = data.get(output_label)
+        unhandled_input_labels.remove(output_label)
+        conversation_section += format_data(output_value, output_ff,  ctx=ctx+f": generate_conversation output {output_label}")
+
+    if len(unhandled_input_labels) > 0:
+        raise ValueError(ctx+f"got input values with unknown labels").add_note(f"unknown labels: {unhandled_input_labels}")
+
     return conversation_section
 
 
@@ -110,7 +119,7 @@ def generate_sys_prompt(
         rules: List[str] = [],
         inputs: List[Tuple[str, FFFF]],
         output: Tuple[str, FFFF],
-        good_examples: List[Tuple],
+        good_examples: List[Dict[str, Any]],
         **kwargs) -> str:
     
     prompt_head = f"{stage_description}\n"
@@ -133,7 +142,7 @@ def generate_sys_prompt(
 
     good_examples_section = "# GOOD Examples\n\n" if len(good_examples) > 0 else ""
     for example in good_examples:
-        good_examples_section += generate_conversation(*example, inputs=inputs, output=output, with_output=True) + "\n"
+        good_examples_section += generate_conversation(example, inputs=inputs, output=output, with_output=True, ctx="system_prompt") + "\n"
 
     sys_prompt = "\n".join([sec for sec in [prompt_head, rules_section, format_section, good_examples_section, "# Conversation\n"] if sec != ""])
 
@@ -151,22 +160,21 @@ class Stage:
     tries: int
     llm: LLM
 
-    def __call__(self, *args, tries: int|None = None):
-        if len(args) != len(self.inputs):
-            raise ValueError(f"stage_call_error: {self.name}: incompatible number of input arguments")
+    def __call__(self, input_data: Dict[str, Any], tries: int|None = None, llm: LLM|None = None, tracer: Tracer = ZeroTracer()):
 
-        conversation_section = generate_conversation(*args, inputs=self.inputs, output=self.output)
+        conversation_section = generate_conversation(input_data, inputs=self.inputs, output=self.output, ctx="stage_run")
 
         for try_nr in range(tries or self.tries):
             context = [
                 CtxMessage(Role.System, self.sys_prompt),
                 CtxMessage(Role.User, conversation_section)
             ]
-            llm_response = call_llm(context, self.llm)
+            llm_response = call_llm(context, llm or self.llm)
             
             deserialized = llm_response if self.output[1].encoding == Encoding.rawtextline else json.loads(llm_response)
             return deserialized
 
+        return None # if no try was successful
 
 
 def make_stage(
@@ -176,12 +184,10 @@ def make_stage(
         rules: List[str] = [],
         inputs: List[Tuple[str, FFFF]],
         output: Tuple[str, FFFF],
-        tracer: Tracer,
-        llm: LLM,
-        tries: int = 0,
+        llm: LLM = LLM(""),
+        tries: int = 1,
         good_examples: List[Tuple]) -> Stage:
     args = locals()
-    local_tracer = tracer.on("stage_" + stage_name, {})
 
     sys_prompt = generate_sys_prompt(**args)
     stage = Stage(
