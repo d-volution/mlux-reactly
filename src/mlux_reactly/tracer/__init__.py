c3917c4b3020ac225a6b306bcdf60440cf3cdb05
Donovan <it.don@pm.me>
2026-02-03 23:51:02 +0100

    fix: import LIVE_VERBOSE, and format_event

--- src/mlux_reactly/tracer/__init__.py
+++ src/mlux_reactly/tracer/__init__.py
@@ -1,1 +1,1 @@
-from .test_tracer import TestTracer, Event, make_json_serializable, format_tracer, format_tracer_with_nr, FormatConfig, TraceConfig
+from .test_tracer import TestTracer, Event, make_json_serializable, format_tracer, format_tracer_with_nr, FormatConfig, TraceConfig, LIVE_VERBOSE, format_event
